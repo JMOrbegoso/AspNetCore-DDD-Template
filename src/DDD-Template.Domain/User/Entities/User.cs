3ace7786d4a6169f301305ceb98741b2a67f34a8
JM Orbegoso <dev@jmorbegoso.com>
2021-08-19 18:53:13 -0500

    Create User.cs

---
+++ src/DDD-Template.Domain/User/Entities/User.cs
@@ -0,0 +1,45 @@
+﻿using DDD_Template.Domain.Base;
+using DDD_Template.Domain.User.ValueObjects;
+using System;
+
+namespace DDD_Template.Domain.User.Entities
+{
+    public sealed class User : Entity
+    {
+        public FirstName FirstName { get; private set; }
+        public LastName LastName { get; private set; }
+
+        private User(Guid id, string firstName, string lastName) : base(id)
+        {
+            this.FirstName = FirstName.Create(firstName);
+            this.LastName = LastName.Create(lastName);
+        }
+
+        public static User Create(string firstName, string lastName)
+        {
+            return new User(Guid.NewGuid(), firstName, lastName);
+        }
+        public static User Create(Guid id, string firstName, string lastName)
+        {
+            return new User(id, firstName, lastName);
+        }
+
+        public void UpdateFirstName(string firstName)
+        {
+            this.FirstName = FirstName.Create(firstName);
+        }
+        public void UpdateFirstName(FirstName firstName)
+        {
+            this.FirstName = firstName;
+        }
+
+        public void UpdateLastName(string lastName)
+        {
+            this.LastName = LastName.Create(lastName);
+        }
+        public void UpdateLastName(LastName lastName)
+        {
+            this.LastName = lastName;
+        }
+    }
+}
