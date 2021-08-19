574a544fbca6ba88a227122417c4d1d2857132cf
JM Orbegoso <dev@jmorbegoso.com>
2021-08-19 11:18:41 -0500

    Create ValueObject.cs

---
+++ src/DDD-Template.Domain/Base/ValueObject.cs
@@ -0,0 +1,16 @@
+﻿namespace DDD_Template.Domain.Base
+{
+    public abstract record ValueObject<T>
+    {
+        public T Value { get; }
+
+        protected ValueObject(T value)
+        {
+            this.Validate(value);
+
+            this.Value = value;
+        }
+
+        protected abstract void Validate(T value);
+    }
+}
