d40ff8dbe8b9e0c0d1abd3109304862d8515a515
JM Orbegoso <dev@jmorbegoso.com>
2021-08-19 18:48:48 -0500

    Create Entity.cs

---
+++ src/DDD-Template.Domain/Base/Entity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DDD_Template.Domain.Base
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; }
+
+        protected Entity(Guid id)
+        {
+            this.Id = id;
+        }
+
+        public bool Equals(Entity other)
+        {
+            if (other is null) return false;
+
+            if (this.GetType() != other.GetType()) return false;
+
+            if (ReferenceEquals(this, other)) return true;
+
+            return this.Id.Equals(other.Id);
+        }
+
+        public override bool Equals(object obj) => this.Equals(obj as Entity);
+
+        public static bool operator ==(Entity a, Entity b) => a.Equals(b);
+
+        public static bool operator !=(Entity a, Entity b) => !a.Equals(b);
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
