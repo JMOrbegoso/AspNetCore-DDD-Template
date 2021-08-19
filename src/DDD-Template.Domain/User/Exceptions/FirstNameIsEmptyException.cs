efd4b425e5fe62572901e0c9e7f6d32f471f0923
JM Orbegoso <dev@jmorbegoso.com>
2021-08-19 11:20:39 -0500

    Create FirstNameIsEmptyException.cs

---
+++ src/DDD-Template.Domain/User/Exceptions/FirstNameIsEmptyException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace DDD_Template.Domain.User.Exceptions
+{
+    public class FirstNameIsEmptyException : ArgumentNullException
+    {
+        public FirstNameIsEmptyException() : base("FirstName") { }
+    }
+}
