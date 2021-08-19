35d8c1186b39003c573bd7ba62198012cb459ccf
JM Orbegoso <dev@jmorbegoso.com>
2021-08-19 11:19:45 -0500

    Create FirstName.cs

---
+++ src/DDD-Template.Domain/User/ValueObjects/FirstName.cs
@@ -0,0 +1,31 @@
+﻿using DDD_Template.Domain.Base;
+using DDD_Template.Domain.User.Exceptions;
+
+namespace DDD_Template.Domain.User.ValueObjects
+{
+    public sealed record FirstName : ValueObject<string>
+    {
+        public readonly static int MaxLength = 255;
+
+        private FirstName(string value) : base(value) { }
+
+        public static FirstName Create(string value)
+        {
+            return new FirstName(value);
+        }
+
+        protected override void Validate(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new FirstNameIsEmptyException();
+
+            if (value.Length > FirstName.MaxLength)
+                throw new FirstNameIsTooLongException();
+        }
+
+        public override string ToString()
+        {
+            return this.Value;
+        }
+    }
+}
