fdff93371b6806a70ab4a6a99dd9e93d167410ba
JM Orbegoso <dev@jmorbegoso.com>
2021-08-19 19:15:21 -0500

    Update UserTest.cs Add tests: Expected_Update_FirstName and Expected_Update_LastName.

--- tests/DDD-Template.Domain.UnitTests/User/Entities/UserTest.cs
+++ tests/DDD-Template.Domain.UnitTests/User/Entities/UserTest.cs
@@ -38,5 +38,43 @@ namespace DDD_Template.Domain.UnitTests.User.Entities
             user.FirstName.Value.Should().Be(firstName);
             user.LastName.Value.Should().Be(lastName);
         }
+
+        [Fact]
+        public void Expected_Update_FirstName()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+            var originalFirstName = "John";
+            var newFirstName = "Johny";
+            var lastName = "Doe";
+
+            // Act
+            var user = Domain.User.Entities.User.Create(id, originalFirstName, lastName);
+            user.UpdateFirstName(newFirstName);
+
+            // Assert
+            user.Id.Should().Be(id);
+            user.FirstName.Value.Should().Be(newFirstName);
+            user.LastName.Value.Should().Be(lastName);
+        }
+
+        [Fact]
+        public void Expected_Update_LastName()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+            var firstName = "John";
+            var originalLastName = "Doe";
+            var newLastName = "Doeh";
+
+            // Act
+            var user = Domain.User.Entities.User.Create(id, firstName, originalLastName);
+            user.UpdateLastName(newLastName);
+
+            // Assert
+            user.Id.Should().Be(id);
+            user.FirstName.Value.Should().Be(firstName);
+            user.LastName.Value.Should().Be(newLastName);
+        }
     }
 }
