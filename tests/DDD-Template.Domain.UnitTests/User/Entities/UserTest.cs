d22e937021f4f7a2ffe4c7fc4f8886f3b1a63c5b
JM Orbegoso <dev@jmorbegoso.com>
2021-08-19 19:28:24 -0500

    Update UserTest.cs Add tests: Expected_Same_User, Expected_Same_User_using_Operator, Expected_Different_User and Expected_Different_User_using_Operator.

--- tests/DDD-Template.Domain.UnitTests/User/Entities/UserTest.cs
+++ tests/DDD-Template.Domain.UnitTests/User/Entities/UserTest.cs
@@ -76,5 +76,79 @@ namespace DDD_Template.Domain.UnitTests.User.Entities
             user.FirstName.Value.Should().Be(firstName);
             user.LastName.Value.Should().Be(newLastName);
         }
+
+        [Fact]
+        public void Expected_Same_User()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+            var user1FirstName = "John";
+            var user2FirstName = "Johny";
+            var user1LastName = "Doe";
+            var user2LastName = "Doeh";
+
+            // Act
+            var user1 = Domain.User.Entities.User.Create(id, user1FirstName, user1LastName);
+            var user2 = Domain.User.Entities.User.Create(id, user2FirstName, user2LastName);
+            var equality = user1.Equals(user2);
+
+            // Assert
+            equality.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Expected_Same_User_using_Operator()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+            var user1FirstName = "John";
+            var user2FirstName = "Johny";
+            var user1LastName = "Doe";
+            var user2LastName = "Doeh";
+
+            // Act
+            var user1 = Domain.User.Entities.User.Create(id, user1FirstName, user1LastName);
+            var user2 = Domain.User.Entities.User.Create(id, user2FirstName, user2LastName);
+            var equality = user1 == user2;
+
+            // Assert
+            equality.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Expected_Different_User()
+        {
+            // Arrange
+            var user1Id = Guid.NewGuid();
+            var user2Id = Guid.NewGuid();
+            var firstName = "John";
+            var lastName = "Doe";
+
+            // Act
+            var user1 = Domain.User.Entities.User.Create(user1Id, firstName, lastName);
+            var user2 = Domain.User.Entities.User.Create(user2Id, firstName, lastName);
+            var equality = user1.Equals(user2);
+
+            // Assert
+            equality.Should().BeFalse();
+        }
+
+        [Fact]
+        public void Expected_Different_User_using_Operator()
+        {
+            // Arrange
+            var user1Id = Guid.NewGuid();
+            var user2Id = Guid.NewGuid();
+            var firstName = "John";
+            var lastName = "Doe";
+
+            // Act
+            var user1 = Domain.User.Entities.User.Create(user1Id, firstName, lastName);
+            var user2 = Domain.User.Entities.User.Create(user2Id, firstName, lastName);
+            var equality = user1 == user2;
+
+            // Assert
+            equality.Should().BeFalse();
+        }
     }
 }
