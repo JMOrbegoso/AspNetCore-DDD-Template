d3d42a65435e3fa02226bde4393d23024adf69b7
JM Orbegoso <dev@jmorbegoso.com>
2021-08-19 19:00:56 -0500

    Create UserTest.cs

---
+++ tests/DDD-Template.Domain.UnitTests/User/Entities/UserTest.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using System;
+using Xunit;
+
+namespace DDD_Template.Domain.UnitTests.User.Entities
+{
+    public class UserTest
+    {
+        [Fact]
+        public void Expected_the_same_Id_FirstName_LastName()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+            var firstName = "John";
+            var lastName = "Doe";
+
+            // Act
+            var user = Domain.User.Entities.User.Create(id, firstName, lastName);
+
+            // Assert
+            user.Id.Should().Be(id);
+            user.FirstName.Value.Should().Be(firstName);
+            user.LastName.Value.Should().Be(lastName);
+        }
+
+        [Fact]
+        public void Expected_the_same_FirstName_LastName()
+        {
+            // Arrange
+            var firstName = "John";
+            var lastName = "Doe";
+
+            // Act
+            var user = Domain.User.Entities.User.Create(firstName, lastName);
+
+            // Assert
+            user.Id.Should().NotBeEmpty();
+            user.FirstName.Value.Should().Be(firstName);
+            user.LastName.Value.Should().Be(lastName);
+        }
+    }
+}
