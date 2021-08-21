e43ec53b44a794250e1d7922edf154941c8ce2ad
JM Orbegoso <dev@jmorbegoso.com>
2021-08-21 10:17:37 -0500

    Create Email.cs

---
+++ src/DDD-Template.Domain/User/ValueObjects/Email.cs
@@ -0,0 +1,36 @@
+﻿using DDD_Template.Domain.Base;
+using DDD_Template.Domain.User.Exceptions;
+using System.Text.RegularExpressions;
+
+namespace DDD_Template.Domain.User.ValueObjects
+{
+    public sealed record Email : ValueObject<string>
+    {
+        public const int MaxLength = 128;
+        public const string EmailRegex = @"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$";
+
+        private Email(string value) : base(value) { }
+
+        public static Email Create(string value)
+        {
+            return new Email(value);
+        }
+
+        protected override void Validate(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new EmailIsEmptyException();
+
+            if (value.Length > Email.MaxLength)
+                throw new EmailIsTooLongException();
+
+            if (!Regex.IsMatch(value, Email.EmailRegex))
+                throw new EmailFormatException();
+        }
+
+        public override string ToString()
+        {
+            return this.Value;
+        }
+    }
+}
