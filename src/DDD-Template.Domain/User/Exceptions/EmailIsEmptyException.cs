0e75e60951148bb2be94b5e6ff93cb537f3cc08d
JM Orbegoso <dev@jmorbegoso.com>
2021-08-21 10:16:24 -0500

    Create EmailIsEmptyException.cs

---
+++ src/DDD-Template.Domain/User/Exceptions/EmailIsEmptyException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace DDD_Template.Domain.User.Exceptions
+{
+    public class EmailIsEmptyException : ArgumentNullException
+    {
+        public EmailIsEmptyException() : base("Email") { }
+    }
+}
