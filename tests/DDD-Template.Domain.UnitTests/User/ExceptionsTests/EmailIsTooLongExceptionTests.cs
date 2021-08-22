1b309d54de34c975df49488ad0c87d2217780118
JM Orbegoso <dev@jmorbegoso.com>
2021-08-22 10:03:44 -0500

    Create EmailIsTooLongExceptionTests.cs

---
+++ tests/DDD-Template.Domain.UnitTests/User/ExceptionsTests/EmailIsTooLongExceptionTests.cs
@@ -0,0 +1,22 @@
+﻿using DDD_Template.Domain.User.Exceptions;
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace DDD_Template.Domain.UnitTests.User.ExceptionsTests
+{
+    public class EmailIsTooLongExceptionTests
+    {
+        [Fact]
+        public void Expected_throw_exception_with_message()
+        {
+            // Arrange
+
+            // Act
+            var act = new Action(() => throw new EmailIsTooLongException());
+
+            // Assert
+            act.Should().Throw<EmailIsTooLongException>().WithMessage("*Email*");
+        }
+    }
+}
