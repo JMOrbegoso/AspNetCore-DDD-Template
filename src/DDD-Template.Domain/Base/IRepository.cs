a851deb93b0180d5856dedfb4f7bf3772714b09c
JM Orbegoso <dev@jmorbegoso.com>
2021-08-25 20:21:48 -0500

    Create IRepository.cs

---
+++ src/DDD-Template.Domain/Base/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace DDD_Template.Domain.Base
+{
+    public interface IRepository<TEntity> where TEntity : Entity
+    {
+        Task<bool> Exist(Guid id);
+
+        Task<IEnumerable<TEntity>> Get();
+
+        Task<TEntity> Get(Guid id);
+
+        Task<IEnumerable<TEntity>> FindAll(Expression<Func<TEntity, bool>> predicate);
+
+        void Add(TEntity entity);
+
+        void Remove(TEntity entity);
+    }
+}
