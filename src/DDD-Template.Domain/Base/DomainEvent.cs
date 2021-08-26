236a2d0e5abf15c0c6c9600a38b83629b18ab9ba
JM Orbegoso <dev@jmorbegoso.com>
2021-08-25 20:21:16 -0500

    Create DomainEvent.cs

---
+++ src/DDD-Template.Domain/Base/DomainEvent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DDD_Template.Domain.Base
+{
+    public abstract record DomainEvent
+    {
+        public Guid Id { get; } = Guid.NewGuid();
+
+        public DateTime DateTime { get; } = DateTime.UtcNow;
+    }
+}
