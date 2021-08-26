db76da93cf57c327a058dcde9b4443ce796c6ffc
JM Orbegoso <dev@jmorbegoso.com>
2021-08-25 20:21:05 -0500

    Create AggregateRoot.cs

---
+++ src/DDD-Template.Domain/Base/AggregateRoot.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DDD_Template.Domain.Base
+{
+    public abstract class AggregateRoot : Entity
+    {
+        private readonly List<DomainEvent> _domainEvents = new List<DomainEvent>();
+        public IReadOnlyList<DomainEvent> GetDomainEvents() => this._domainEvents.AsReadOnly();
+
+        public AggregateRoot(Guid id) : base(id) { }
+
+        protected void AddDomainEvent(DomainEvent domainEvent)
+        {
+            this._domainEvents.Add(domainEvent);
+        }
+
+        protected void ClearDomainEvents()
+        {
+            this._domainEvents.Clear();
+        }
+    }
+}
