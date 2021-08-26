433f5ed03abae54d0a8677328408a0021e8ff843
JM Orbegoso <dev@jmorbegoso.com>
2021-08-25 20:21:58 -0500

    Create IUsersRepository.cs

---
+++ src/DDD-Template.Domain/Users/Repositories/IUsersRepository.cs
@@ -0,0 +1,10 @@
+﻿using DDD_Template.Domain.Base;
+using DDD_Template.Domain.Users.Entities;
+
+namespace DDD_Template.Domain.Users.Repositories
+{
+    public interface IUsersRepository : IRepository<User>
+    {
+
+    }
+}
