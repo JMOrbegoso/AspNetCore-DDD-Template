37a942f235ccb03ffc7549c5795f772af42f64d5
JM Orbegoso <dev@jmorbegoso.com>
2021-08-25 20:22:08 -0500

    Create IUnitOfWork.cs

---
+++ src/DDD-Template.Domain/Base/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using DDD_Template.Domain.Users.Repositories;
+using System;
+using System.Threading.Tasks;
+
+namespace DDD_Template.Domain.Base
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IUsersRepository UsersRepository { get; }
+
+        Task CommitAsync();
+    }
+}
