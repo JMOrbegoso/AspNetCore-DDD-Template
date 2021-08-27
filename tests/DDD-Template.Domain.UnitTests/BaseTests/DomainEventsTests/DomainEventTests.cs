76845afbae9ac4303ef333737472913d6c907706
JM Orbegoso <dev@jmorbegoso.com>
2021-08-27 10:57:35 -0500

    Create DomainEventTests.cs

---
+++ tests/DDD-Template.Domain.UnitTests/BaseTests/DomainEventsTests/DomainEventTests.cs
@@ -0,0 +1,31 @@
+﻿using DDD_Template.Domain.Base.DomainEvents;
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace DDD_Template.Domain.UnitTests.BaseTests.DomainEventsTests
+{
+    public class DomainEventTests
+    {
+        private record CreatedCustomerDomainEvent : DomainEvent
+        {
+
+        }
+
+        [Fact]
+        public void Expected_Create_CreatedCustomerDomainEvent()
+        {
+            // Arrange
+
+            // Act
+            var domainEvent = new CreatedCustomerDomainEvent();
+
+            // Assert
+            domainEvent.Should().BeAssignableTo(typeof(IDomainEvent));
+            domainEvent.Should().BeOfType(typeof(CreatedCustomerDomainEvent));
+
+            domainEvent.Id.Should().NotBeEmpty();
+            domainEvent.CreatedAtUtc.Should().BeCloseTo(DateTime.UtcNow, TimeSpan.FromSeconds(10));
+        }
+    }
+}
