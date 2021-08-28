3c594e27057e1c7f2c1ded7799fa220c1b1ae2fc
JM Orbegoso <dev@jmorbegoso.com>
2021-08-28 11:27:07 -0500

    Generate first migration

---
+++ src/DDD-Template.Infrastructure/Persistence/Migrations/20210828162551_InitialSchema.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DDD_Template.Infrastructure.Persistence.Migrations
+{
+    public partial class InitialSchema : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "User");
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                schema: "User",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: true),
+                    BirthDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "User",
+                schema: "User");
+        }
+    }
+}
