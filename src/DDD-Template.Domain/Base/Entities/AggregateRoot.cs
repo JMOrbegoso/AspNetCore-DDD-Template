480bf43ba5fc5013444e2075aeb38b011417ecb2
JM Orbegoso <dev@jmorbegoso.com>
2021-08-28 10:20:55 -0500

    Update AggregateRoot.cs Make constructor protected.

--- src/DDD-Template.Domain/Base/Entities/AggregateRoot.cs
+++ src/DDD-Template.Domain/Base/Entities/AggregateRoot.cs
@@ -9,7 +9,7 @@ namespace DDD_Template.Domain.Base.Entities
         private readonly List<IDomainEvent> _domainEvents = new List<IDomainEvent>();
         public IReadOnlyList<IDomainEvent> DomainEvents { get => this._domainEvents.AsReadOnly(); }
 
-        public AggregateRoot(Guid id) : base(id) { }
+        protected AggregateRoot(Guid id) : base(id) { }
 
         public void AddDomainEvent(IDomainEvent domainEvent)
         {
