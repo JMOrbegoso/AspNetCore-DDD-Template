b181193a820c7ae47bd52613f19040decaf22127
JM Orbegoso <dev@jmorbegoso.com>
2021-08-28 19:30:06 -0500

    Create UnitOfWork.cs

---
+++ src/DDD-Template.Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,33 @@
+﻿using DDD_Template.Domain.Base.Repositories;
+using DDD_Template.Domain.Users.Repositories;
+using DDD_Template.Infrastructure.Persistence.Contexts;
+using System;
+using System.Threading.Tasks;
+
+namespace DDD_Template.Infrastructure.Repositories
+{
+    public sealed class UnitOfWork : IUnitOfWork
+    {
+        private ApplicationDbContext _applicationDbContext { get; }
+
+        public IUsersRepository UsersRepository { get; }
+
+        public UnitOfWork(ApplicationDbContext applicationDbContext)
+        {
+            this._applicationDbContext = applicationDbContext;
+
+            this.UsersRepository = new UsersRepository(applicationDbContext);
+        }
+
+        public async Task<int> CommitChangesAsync()
+        {
+            return await this._applicationDbContext.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            this._applicationDbContext.Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
