9bbea3d07e974b1dac544546be234161f6f13c2a
JM Orbegoso <dev@jmorbegoso.com>
2021-08-28 21:12:45 -0500

    Create MediatrDomainEventDispatcher.cs

---
+++ src/DDD-Template.Application/DomainEvents/MediatrDomainEventDispatcher.cs
@@ -0,0 +1,32 @@
+﻿using DDD_Template.Domain.Base.DomainEvents;
+using MediatR;
+using System;
+using System.Threading.Tasks;
+
+namespace DDD_Template.Application.DomainEvents
+{
+    public sealed class MediatrDomainEventDispatcher : IDomainEventDispatcher
+    {
+        private readonly IMediator _mediator;
+
+        public MediatrDomainEventDispatcher(IMediator mediator)
+        {
+            this._mediator = mediator;
+        }
+
+        public async Task Dispatch(IDomainEvent domainEvent)
+        {
+            var domainEventNotification = this.CreateDomainEventNotification(domainEvent);
+
+            await this._mediator.Publish(domainEventNotification);
+        }
+
+        public INotification CreateDomainEventNotification(IDomainEvent domainEvent)
+        {
+            var domainEventType = domainEvent.GetType();
+            var genericDispatcherType = typeof(DomainEventNotification<>).MakeGenericType(domainEventType);
+            var notification = Activator.CreateInstance(genericDispatcherType, domainEvent);
+            return (INotification)notification;
+        }
+    }
+}
