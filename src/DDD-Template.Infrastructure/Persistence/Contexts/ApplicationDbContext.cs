0142396cdc3cc2831916423aceb8f33a26b4f188
JM Orbegoso <dev@jmorbegoso.com>
2021-08-28 19:12:16 -0500

    Update ApplicationDbContext.cs Remove Users property.

--- src/DDD-Template.Infrastructure/Persistence/Contexts/ApplicationDbContext.cs
+++ src/DDD-Template.Infrastructure/Persistence/Contexts/ApplicationDbContext.cs
@@ -1,13 +1,10 @@
-﻿using DDD_Template.Domain.Users.Entities;
-using DDD_Template.Infrastructure.Persistence.EntityTypeConfigurations;
+﻿using DDD_Template.Infrastructure.Persistence.EntityTypeConfigurations;
 using Microsoft.EntityFrameworkCore;
 
 namespace DDD_Template.Infrastructure.Persistence.Contexts
 {
     public class ApplicationDbContext : DbContext
     {
-        public DbSet<User> Users { get; set; }
-
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
