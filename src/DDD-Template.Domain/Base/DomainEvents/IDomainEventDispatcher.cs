ad39d3ce93cddd8ab28345e81c513a9762577b4a
JM Orbegoso <dev@jmorbegoso.com>
2021-08-28 21:11:12 -0500

    Create IDomainEventDispatcher.cs

---
+++ src/DDD-Template.Domain/Base/DomainEvents/IDomainEventDispatcher.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace DDD_Template.Domain.Base.DomainEvents
+{
+    public interface IDomainEventDispatcher
+    {
+        Task Dispatch(IDomainEvent domainEvent);
+    }
+}
