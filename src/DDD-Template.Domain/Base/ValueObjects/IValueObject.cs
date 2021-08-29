1cf749177d073ac22ed101aff71052d4d66d72e2
JM Orbegoso <dev@jmorbegoso.com>
2021-08-29 13:59:49 -0500

    Update IValueObject.cs Extend from IEquatable<T>.

--- src/DDD-Template.Domain/Base/ValueObjects/IValueObject.cs
+++ src/DDD-Template.Domain/Base/ValueObjects/IValueObject.cs
@@ -2,7 +2,7 @@
 
 namespace DDD_Template.Domain.Base.ValueObjects
 {
-    public interface IValueObject<T> where T : IEquatable<T>
+    public interface IValueObject<T> : IEquatable<T> where T : IEquatable<T>
     {
         T Value { get; }
     }
