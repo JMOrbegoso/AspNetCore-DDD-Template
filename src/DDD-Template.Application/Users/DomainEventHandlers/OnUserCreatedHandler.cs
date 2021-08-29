9b75363eddfc7e720745108ad4b396fd1899c323
JM Orbegoso <dev@jmorbegoso.com>
2021-08-29 09:39:33 -0500

    Create OnUserCreatedHandler.cs

---
+++ src/DDD-Template.Application/Users/DomainEventHandlers/OnUserCreatedHandler.cs
@@ -0,0 +1,21 @@
+﻿using DDD_Template.Application.DomainEvents;
+using DDD_Template.Domain.Users.DomainEvents;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DDD_Template.Application.Users.DomainEventHandlers
+{
+    public class OnUserCreatedHandler : INotificationHandler<DomainEventNotification<UserCreatedDomainEvent>>
+    {
+        public OnUserCreatedHandler()
+        {
+
+        }
+
+        public Task Handle(DomainEventNotification<UserCreatedDomainEvent> notification, CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
